d941b6b9b5df1350bf9df7f73470637d3d8233df
Seven <415849169@qq.com>
2024-08-03 14:31:40 +0800

    添加sql语句

---
+++ 05-mybatis-mysql-druid/src/main/resources/sql/user.sql
@@ -0,0 +1,37 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql5
+ Source Server Type    : MySQL
+ Source Server Version : 50739
+ Source Host           : localhost:3307
+ Source Schema         : test_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 50739
+ File Encoding         : 65001
+
+ Date: 03/08/2024 14:30:47
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `age` int(11) NULL DEFAULT NULL,
+  `gender` tinyint(4) NULL DEFAULT NULL COMMENT '性别',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'seven', 18, 0);
+
+SET FOREIGN_KEY_CHECKS = 1;
