58ef5112e26e8e39d5f296e8512b9ea2bc815df4
Seven <415849169@qq.com>
2024-09-01 12:35:47 +0800

    AOP注解的妙用 - 实现接口限流、防重、防抖

---
+++ 09-aop-limit-repeatsubmit-antishake/src/main/java/com/seven/aoplimitrepeatsubmitantishake/App.java
@@ -0,0 +1,17 @@
+package com.seven.aoplimitrepeatsubmitantishake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class);
+    }
+}
